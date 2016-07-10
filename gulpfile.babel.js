29d8097f0f6e79f7a72eaf317c94e09372617aba
Joao Paulo Barbosa <jpaulobneto@gmail.com>
2016-07-10 00:22:40 -0300

    Add gulp tasks and project boilerplate

---
+++ gulpfile.babel.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import gulp from 'gulp';
+import requireDir from 'require-dir';
+import runSequence from 'run-sequence';
+
+requireDir('./gulp');
+
+// TODO Adjust build
+gulp.task('default', ['clean'], cb => {
+  return runSequence(
+    ['styles', 'vendors', 'scripts'],
+    ['fonts', 'images', 'minifyCss'],
+    'copy',
+    cb
+  );
+});
